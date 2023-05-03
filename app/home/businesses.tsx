4d72bde5c9d1b2d363c081f90900883cf10fa7a8
Randall71 <51382079+Randall71@users.noreply.github.com>
2023-05-03 21:36:54 +0000

    feat: route of tab bar

--- app/home/businesses.tsx
+++ app/home/businesses.tsx
@@ -1,28 +1,37 @@
-import useAsync from "hooks/useAsync";
-import {View , Text} from "react-native";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
-import api from "utils/api";
-import { getBusinesses, myFunction } from "utils/businesses";
-import {scale} from 'react-native-size-scaling'
-
+import useAsync from 'hooks/useAsync'
+import { View, Text } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { scale } from 'react-native-size-scaling'
+import { getBusinesses } from 'utils/businesses'
 
 export default function BussinessesScreen() {
+  const { status, value, error } = useAsync<any>(getBusinesses, true)
 
-    const { status, value, error } = useAsync<any>( getBusinesses, true);
+  if (status === 'idle' || status === 'pending') {
+    return <Text style={{ fontSize: 28 }}>Loading 🚀...</Text>
+  }
 
+  if (status === 'error') {
+    return <Text style={{ fontSize: 32 }}>{error}</Text>
+  }
 
-    if( status === "idle" || status === "pending"    ) {
-<Text style={{fontSize: 32}}>Loading...</Text>
-    }
-
-    if(status === "error" ) {
-<Text style={{fontSize: 32}}>{error}</Text>
-    }
-
-  
-
-    return <FlatList style={{ flex: 1 }} contentContainerStyle={{flex: 1 }} data={value} renderItem={({item}) =><View style={{backgroundColor: "white", padding: 10, 
-    marginVertical: scale(20) , marginHorizontal: scale(15), borderRadius: 10}}>
-        <Text style={{fontSize: 16, fontWeight: 'bold'}}>{item.name}</Text>
-    </View> } />
+  return (
+    <FlatList
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flex: 1 }}
+      data={value}
+      renderItem={({ item }) => (
+        <View
+          style={{
+            backgroundColor: 'white',
+            padding: 10,
+            marginVertical: scale(20),
+            marginHorizontal: scale(15),
+            borderRadius: 10,
+          }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{item.name}</Text>
+        </View>
+      )}
+    />
+  )
 }
--- app/home/services.tsx
+++ app/home/services.tsx
@@ -1,25 +1,63 @@
-import useAsync from "hooks/useAsync";
-import { Text, View } from "react-native";
-import { getServices } from "utils/services";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
-import {scale} from 'react-native-size-scaling'
+import { useRouter } from 'expo-router'
+import useAsync from 'hooks/useAsync'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
+import { FlatList, ScrollView } from 'react-native-gesture-handler'
+import { scale } from 'react-native-size-scaling'
+import { API_URL } from 'utils/api'
+import { getServices } from 'utils/services'
 
 export default function ServicesScreen() {
-  const { status, value, error } = useAsync<any>( getServices, true);
+  const { status, value, error } = useAsync<any>(getServices, true)
 
+  const router = useRouter()
 
-  if( status === "idle" || status === "pending"    ) {
-<Text style={{fontSize: 32}}>Loading...</Text>
+  if (status === 'idle' || status === 'pending') {
+    return <Text style={{ fontSize: 28 }}>Loading 🚀...</Text>
   }
 
-  if(status === "error" ) {
-<Text style={{fontSize: 32}}>{error}</Text>
+  if (status === 'error') {
+    return <Text style={{ fontSize: 32 }}>{error}</Text>
   }
 
-
-
-  return <FlatList style={{ flex: 1 }} contentContainerStyle={{ }} data={value} renderItem={({item}) =><View style={{backgroundColor: "white", padding: 10, 
-  marginVertical: scale(20) , marginHorizontal: scale(15), borderRadius: 10}}>
-      <Text style={{fontSize: 16, fontWeight: 'bold'}}>{item.name}</Text>
-  </View> } />
+  return (
+    <FlatList
+      style={{ flex: 1 }}
+      data={value}
+      renderItem={({ item }) => {
+        // console.log(`${API_URL}/${item.image}`, 'IMAGEEEEE')
+        return (
+          <TouchableOpacity
+            onPress={() =>
+              router.push({
+                pathname: '/service/[service]',
+                params: { service: item._id },
+              })
+            }
+            style={{
+              backgroundColor: 'white',
+              padding: 10,
+              marginVertical: scale(20),
+              marginHorizontal: scale(15),
+              borderRadius: 10,
+            }}>
+            <Image
+              source={{
+                uri: `${API_URL}/${item.image}`,
+                headers: {
+                  'application-key': 'MRZGFPLYPXMETWJBTV2Z84AR',
+                  Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDUyNGFlMzM1YWM0MjA4YjZhNDRiNzUiLCJmaXJzdF9uYW1lIjoiVGVzdCBRdWlja2RvIiwibGFzdF9uYW1lIjoibGFzdCBuYW1lIHF1aWNrZG8iLCJlbWFpbCI6Inlhbm5waGlwcDE3QGdtYWlsLmNvbSIsImNvdW50cnkiOiJjYW1lcm9vbiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4MzExNDcyMywiZXhwIjoxNjgzMjg3NTIzfQ.oghrPdi_R6KwslLsnE-5vpRK95XystUdiyGgVCMUnbw`,
+                },
+                width: 300,
+                height: 200,
+              }}
+              style={{ height: 100, width: 100 }}
+            />
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )
+      }}
+    />
+  )
 }
