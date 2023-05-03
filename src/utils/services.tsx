1487e55e5ff2f28c6072caa167517116e6468961
Randall71 <51382079+Randall71@users.noreply.github.com>
2023-05-03 21:38:37 +0000

    fix: utility for fetch services

--- src/utils/services.tsx
+++ src/utils/services.tsx
@@ -1,11 +1,17 @@
-import api from "./api"
+import api from './api'
 
 export const getServices = async () => {
+  const request = await api('/services', 'GET', { undefined })
 
+  const response = await request.json()
 
-    const request = await api('/services', 'GET' , {undefined} )
+  return response
+}
 
-    const response = await request.json()
+export const getServiceDetail = async (id: string) => {
+  const request = await api(`/services/${id}`, 'GET', { undefined })
 
-    return response
+  const response = await request.json()
+
+  return await response
 }
