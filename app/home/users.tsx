6e8bb71db7701eefc1ecaac560bdf65a54d0f758
Randall71 <51382079+Randall71@users.noreply.github.com>
2023-05-03 22:30:21 +0000

    chore: lint

--- app/home/users.tsx
+++ app/home/users.tsx
@@ -1,34 +1,44 @@
-import useAsync from "hooks/useAsync";
-import { Text, View } from "react-native";
-import { getServices } from "utils/services";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
-import {scale} from 'react-native-size-scaling'
-import { getUsers } from "utils/users";
+import useAsync from 'hooks/useAsync'
+import { Text, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { scale } from 'react-native-size-scaling'
+import { getUsers } from 'utils/users'
 
 export default function UsersScreen() {
-    const { status, value, error } = useAsync<any>( getUsers, true);
+  const { status, value, error } = useAsync<any>(getUsers, true)
 
-
-  if( status === "idle" || status === "pending"    ) {
-<Text style={{fontSize: 32}}>Loading...</Text>
+  if (status === 'idle' || status === 'pending') {
+    return <Text style={{ fontSize: 32 }}>Loading...</Text>
   }
 
-  if(status === "error" ) {
-<Text style={{fontSize: 32}}>{error}</Text>
+  if (status === 'error') {
+    return <Text style={{ fontSize: 32 }}>{error} 😞</Text>
   }
 
-
-
-  return <FlatList style={{ flex: 1 }} contentContainerStyle={{ }} data={value} 
-  
-  renderItem={({item}) =><View style={{backgroundColor: "white", padding: 10, 
-  marginVertical: scale(20) , marginHorizontal: scale(15), borderRadius: 10 , }}>
-    <View >
-    <Text style={{fontSize: 16, fontWeight: 'bold'}}>{item.last_name} {item.first_name}</Text>
-
-    </View>
-    <View style={{flexDirection: 'row', paddingTop: scale(5)}}>
-        <Text>{item.email}</Text>
-    </View>
-  </View> } />
+  return (
+    <FlatList
+      style={{ flex: 1 }}
+      contentContainerStyle={{}}
+      data={value}
+      renderItem={({ item }) => (
+        <View
+          style={{
+            backgroundColor: 'white',
+            padding: 10,
+            marginVertical: scale(20),
+            marginHorizontal: scale(15),
+            borderRadius: 10,
+          }}>
+          <View>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+              {item.last_name} {item.first_name}
+            </Text>
+          </View>
+          <View style={{ flexDirection: 'row', paddingTop: scale(5) }}>
+            <Text>{item.email}</Text>
+          </View>
+        </View>
+      )}
+    />
+  )
 }
